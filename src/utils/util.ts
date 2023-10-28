9766189fe66daafafb96caaea66b58d69166cc23
MartinsOnuoha <martinsvictor.nd@gmail.com>
2023-10-28 16:30:29 +0100

    ✨ feat(appcategory.vue): add randomized category colors

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,17 +1,3 @@
-export function shuffle(t: Array<any>) {
-  let n
-  let last = t.length
-  while (last > 0) {
-    n = rand(last)
-    swap(t, n, --last)
-  }
-  return t
-}
-const rand = (n: number) => 0 | (Math.random() * n)
-
-function swap(t: Array<any>, i: number, j: number) {
-  const q = t[i]
-  t[i] = t[j]
-  t[j] = q
-  return t
+export function randomChoice<T>(arr: Array<T>): T {
+  return arr[Math.floor(Math.random() * arr.length)]
 }
